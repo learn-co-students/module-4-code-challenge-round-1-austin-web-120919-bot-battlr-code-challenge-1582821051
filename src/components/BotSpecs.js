122a1545913719d7b130df51dc24e4adfd2329b8
Chance Mims <chance@Chances-MacBook-Air.local>
2020-02-27 11:57:03 -0600

    added enlist-button feature

--- src/components/BotSpecs.js
+++ src/components/BotSpecs.js
@@ -60,18 +60,18 @@ const BotSpecs = props => {
             </div>
             <button
               className="ui button fluid"
-              onClick={() =>
-                console.log('connect this to a function that shows all bots')
+              onClick={(e) =>
+                props.handleClick(e)
               }
             >
               Go Back
             </button>
             <button
+              id={bot.id}
               className="ui button fluid"
-              onClick={() =>
-                console.log(
-                  "connect this to a function that adds this bot to your bot army list"
-                )
+              onClick={(e) =>
+                props.handleEnlist(e)
+                
               }
             >
               Enlist
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,54 +1,102 @@
 import React from "react";
-import BotCollection from './BotCollection'
-import YourBotArmy from './YourBotArmy'
+import BotCollection from "./BotCollection";
+import YourBotArmy from "./YourBotArmy";
+import BotSpecs from "../components/BotSpecs";
 
 class BotsPage extends React.Component {
   //start here with your code for step one
 
-  constructor(){
+  constructor() {
     super();
     this.state = {
       bots: [],
-      yourBots: []
-    }
+      yourBots: [],
+      showDetail: false,
+      currentBot: {}
+    };
   }
 
-  componentDidMount(){
-    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
-    .then(resp => resp.json())
-    .then(json => this.setState({
-      bots: json
-    }))
+  componentDidMount() {
+    fetch("https://bot-battler-api.herokuapp.com/api/v1/bots")
+      .then(resp => resp.json())
+      .then(json =>
+        this.setState({
+          bots: json
+        })
+      );
   }
+
+  // handleClick = e => {
+  //   const botId = e.target.parentNode.parentNode.parentNode.id;
+  //   const foundBot = this.state.bots.find(bot => bot.id == botId)
+  //   //console.log(e.target.parentNode.parentNode.parentNode.id)
+  //   if(this.state.yourBots.includes(foundBot)){
+  //     const botIndex = this.state.yourBots.findIndex(bot => bot.id == foundBot.id)
+  //     this.setState({
+  //       yourBots: ([...this.state.yourBots.slice(0, botIndex), ...this.state.yourBots.slice(botIndex + 1, this.state.yourBots.length)])
+  //     })
+  //   }
+  //   else{
+  //     //console.log(this.state.bots[botId])
+  //     this.setState({
+  //       yourBots: [...this.state.yourBots, foundBot]
+  //     })
+  //   }
+
+  // }
+
+  handleEnlist = e => {
+    const botId = e.target.id;
+    const foundBot = this.state.bots.find(bot => bot.id == botId);
+    console.log(foundBot)
+    if (this.state.yourBots.includes(foundBot)) {
+      const botIndex = this.state.yourBots.findIndex(
+        bot => bot.id == foundBot.id
+      );
+      this.setState({
+        yourBots: [
+          ...this.state.yourBots.slice(0, botIndex),
+          ...this.state.yourBots.slice(botIndex + 1, this.state.yourBots.length)
+        ]
+      });
+    } else {
+      this.setState({
+        yourBots: [...this.state.yourBots, foundBot]
+      });
+    }
+  };
 
   handleClick = e => {
     const botId = e.target.parentNode.parentNode.parentNode.id;
-    const foundBot = this.state.bots.find(bot => bot.id == botId)
-    //console.log(e.target.parentNode.parentNode.parentNode.id)
-    if(this.state.yourBots.includes(foundBot)){
-      const botIndex = this.state.yourBots.findIndex(bot => bot.id == foundBot.id)
-      this.setState({
-        yourBots: ([...this.state.yourBots.slice(0, botIndex), ...this.state.yourBots.slice(botIndex + 1, this.state.yourBots.length)])
-      })
-    }
-    else{
-      //console.log(this.state.bots[botId])
-      this.setState({
-        yourBots: [...this.state.yourBots, foundBot]
-      })
-    }
-    
-  }
+    const foundBot = this.state.bots.find(bot => bot.id == botId);
+    this.setState({
+      showDetail: !this.state.showDetail,
+      currentBot: foundBot
+    });
+  };
 
   render() {
     return (
       <div>
-        <YourBotArmy handleClick={this.handleClick} yourBots={this.state.yourBots}/>
-        <BotCollection handleClick={this.handleClick} bots={this.state.bots} />
+        <YourBotArmy
+          handleClick={this.handleClick}
+          yourBots={this.state.yourBots}
+        />
+        {this.state.showDetail ? (
+          <BotSpecs
+            bot={this.state.currentBot}
+            handleEnlist={this.handleEnlist}
+            handleClick={this.handleClick}
+          />
+        ) : (
+          <BotCollection
+            handleClick={this.handleClick}
+            bots={this.state.bots}
+          />
+        )}
       </div>
     );
   }
-
 }
 
 export default BotsPage;
